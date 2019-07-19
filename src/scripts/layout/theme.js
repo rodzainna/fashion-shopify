5243219b05b41af2ab58eb2567b4f6cbea90b4ea
Rodzainna Hamisain <rodzainna@Rodzainnas-MacBook-Pro.local>
2019-07-19 13:31:14 +0800

    cont html to shopify template --not dynamic

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -8,6 +8,9 @@ import 'lazysizes/plugins/respimg/ls.respimg';
 import '../../styles/theme.scss';
 import '../../styles/theme.scss.liquid';
 
+import './app';
+import '../sections/header';
+
 import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
 import {cookiesEnabled} from '@shopify/theme-cart';
 
